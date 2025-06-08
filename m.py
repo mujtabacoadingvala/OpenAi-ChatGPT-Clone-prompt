6553e9a654a1b6f46ef7f1b4bfdd711349b8561e
Mujtaba Coading Vala <mujtabashahbaz715@gmail.com>
2025-06-08 01:52:21 -0700

    Add files via upload

---
+++ m.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# Apna GitHub link yahan daalein
+github_link = "https://github.com/mujtabacoadingvala?tab=repositories"
+
+# Browser mein link open kare
+webbrowser.open(github_link)
